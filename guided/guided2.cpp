e2e8441fd9ef3ee92d0baf9c4f11e2aa3069a94b
rennanvra <renna.novaria.12@gmail.com>
2024-04-01 13:49:44 +0700

    Modul 3 Sorting

--- guided/guided2.cpp
+++ guided/guided2.cpp
@@ -6,19 +6,20 @@ void insertion_sort(char arr[], int length) {
     int i, j;
     char tmp;
 
-    for (i = 1; 1 < length; i++) {
+    for (i = 1; i < length; i++) {
         j = i;
 
-        while (j > 0 && arr[j - i] < arr[j]) {
+        while (j > 0 && arr[j - 1] < arr[j]) {
             tmp = arr[j];
             arr[j] = arr[j - 1];
             arr[j - 1] = tmp;
+            j--;
         }//end of while loop
     }//end of for loop
 }
 
-void print_array(char a[], int length) {
 
+void print_array(char a[], int length) {
     for(int i=0; i<length; i++) {
         cout << a[i] << "\t";
     }
@@ -26,12 +27,15 @@ void print_array(char a[], int length) {
 }
 
 int main() {
+    
     int length = 6;
     char a[length] = {'c', 'f', 'a', 'z', 'd', 'p'};
-
-    cout << "Urutan karakter sebelum sorting:" << endl;
+ 
+    cout << "Urutan karakter sebelum sorting: " << endl;
     print_array(a, length);
-
-    cout << "\nUrutan karakter setelah sorting:" << endl;
+ 
+    insertion_sort(a, length);
+    
+    cout << "\nUrutan karakter setelah sorting: " << endl;
     print_array(a, length);
 }
